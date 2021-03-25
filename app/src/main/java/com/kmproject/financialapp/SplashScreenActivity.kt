9c6363c5abe6ced79abb66f75bca5f7634259439
khoirul muttaqin <khoirulmuttaqin08@gmail.com>
2021-03-25 21:10:17 +0700

    add activity

--- app/src/main/java/com/kmproject/financialapp/SplashScreenActivity.kt
+++ app/src/main/java/com/kmproject/financialapp/SplashScreenActivity.kt
@@ -2,10 +2,15 @@ package com.kmproject.financialapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+
+        Button btnStart
+
     }
+
 }
